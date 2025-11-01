c477c1ec3b56577f03d13ad9a07f49f8006f65b6
PauloPereira1995 <146553014+PauloSMP@users.noreply.github.com>
2025-11-01 16:22:31 +0000

    feat: implement get_model

--- traffic/traffic.py
+++ traffic/traffic.py
@@ -80,8 +80,35 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
-
+    #First Convolutional layer
+    input_shape = (IMG_WIDTH, IMG_HEIGHT, 3)
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Conv2D(32, (3,3), activation = "relu", padding="same", input_shape = input_shape))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
+    model.add(tf.keras.layers.Dropout(0.2))
+    #Second Convolutional layer
+    model.add(tf.keras.layers.Conv2D(64, (3,3), activation = "relu", padding='same'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
+    model.add(tf.keras.layers.Dropout(0.2))
+    #Third Convolutional layer
+    model.add(tf.keras.layers.Conv2D(128, (3,3), activation = "relu", padding='same'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
+    model.add(tf.keras.layers.Dropout(0.2))
+    #Fourth Convolutional layer
+    model.add(tf.keras.layers.Conv2D(256, (3,3), activation = "relu", padding='same'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
+    model.add(tf.keras.layers.Dropout(0.2))
+    #First Dense Layer
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(128, activation = "relu"))
+    model.add(tf.keras.layers.Dropout(0.2))
+    #Output Layer
+    model.add(tf.keras.layers.Dense(NUM_CATEGORIES, activation = "softmax"))
+    
+    #compile model
+    model.compile(optimizer = "adam", loss = "categorical_crossentropy", metrics = ["accuracy"])
+    return model
+    
 
 if __name__ == "__main__":
     main()
